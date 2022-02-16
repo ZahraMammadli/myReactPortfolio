09ddc16f59e7b831539895759cd04b494fb55b66
Zahra Mammadli <zahra.mrj@gmail.com>
2022-02-16 20:11:48 +0100

    Start work on the home profile

---
+++ client/src/portfolioContainer/Home/Profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="progile-details">
+          <div className="colz">
+            <a href="#">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-google-plus-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-youtube-square"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
